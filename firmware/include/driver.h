d5411c094fea9f8326c903866df219bac59b1fd5
Maurice Snoeren <macsnoeren@gmail.com>
2024-06-16 23:25:41 +0200

    Added documentation to driver.h about license

--- firmware/include/driver.h
+++ firmware/include/driver.h
@@ -1,14 +1,26 @@
 #pragma once
 /**
+ * This file is part of HackTheBom.
+ * HackTheBom is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ * 
+ * HackTheBom is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with
+ * HackTheBom. If not, see <https://www.gnu.org/licenses/>.
+ * 
  * @file       : driver.h
+ * @author     : Maurice Snoeren (MS)
  * @description: The abstract base class for tasks runned by the SmartSensor board. All tasks should use this class. This file
  *               uses the design approach that starts with interfaces that will be implemented by abstract and concrete classes.
  *               This method provides the interface IDriver.
  * @date       : 24-10-2021
- * @author     : Maurice Snoeren (MS)
  * @version    : 1.0
- * @license    : GNU version 3.0
- * @changes
+ * @updates    : 24-10-2021: Initial code.
+ * @todo       : 
  */
 #include <inttypes.h>
 
